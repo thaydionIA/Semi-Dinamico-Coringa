c973151103e8700676c191a0b9efb9e55f4f672f
PedroHenriqueAzevedo <pedrcandidoenrique@gmail.com>
2025-02-19 15:12:41 -0300

    primeira

--- conexao/conexao_bd.php
+++ conexao/conexao_bd.php
@@ -1,9 +1,9 @@
 <?php
 // Configuração para conexão com o banco de dados
 $host = "localhost";     // Endereço do servidor
-$dbname = "u413819793_imports"; // Nome do banco de dados
-$user = "u413819793_cliente";          // Usuário do banco de dados
-$password = "b&0g=sAI#Q3q";          // Senha do banco de dados
+$dbname = "semi-dinamico-coringa"; // Nome do banco de dados
+$user = "root";          // Usuário do banco de dados
+$password = "";          // Senha do banco de dados
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
